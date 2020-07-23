1b26afabf5e794b57dd3e2ee7cf568d17fc38176
moment0517 <lssc4205@126.com>
2020-07-24 01:02:05 +0800

    Update LargeInteger.py
    增加乘法算法

--- LargeInteger.py
+++ LargeInteger.py
@@ -1,7 +1,6 @@
 class LargeInteger:
 
     def __init__(self, integer_str):
-        self.integer_str = integer_str
         if integer_str[0] == '-':
             self.sign = -1
             array = [int(c) for c in integer_str[1:]]
@@ -13,11 +12,44 @@ class LargeInteger:
             array = [int(c) for c in integer_str]
         self.array = [self.sign * i for i in reversed(array)]
 
+    @property
+    def integer_str(self):
+        array = [str(abs(i)) for i in reversed(self.array)]
+        string = "".join(array)
+        if self.sign == -1:
+            string = '-' + string
+        return string
+
     def __len__(self):
         return len(self.array)
 
     def __repr__(self):
         return self.integer_str
+
+    def __mul__(self, other):
+        if str(self) == '0' or str(other) == '0':
+            return LargeInteger('0')
+        n = max(len(self), len(other))
+        if n < 3:
+            return LargeInteger(str(int(self.integer_str) * int(other.integer_str)))
+        else:
+            m = n // 2
+            x = LargeInteger(self.integer_str)
+            x.array = x.array[m:] if m < len(x) else [0]
+            y = LargeInteger(self.integer_str)
+            y.array = y.array[:m]
+            w = LargeInteger(other.integer_str)
+            w.array = w.array[m:] if m < len(w) else [0]
+            z = LargeInteger(other.integer_str)
+            z.array = z.array[:m]
+
+            part1 = x * w
+            part1.array = [0] * (2*m) + part1.array
+            part2 = x * z + w * y
+            part2.array = [0] * m + part2.array
+            part3 = y * z
+
+            return part1 + part2 + part3
 
     @staticmethod
     def _adjust_array(array):
